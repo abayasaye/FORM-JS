ad181a08aa0f8cb93137ce3082189ef583278bc1
abayasaye <abay400@gmail.com>
2022-06-10 13:48:16 +0300

    form_js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function functionSub() {
-    if (fLNameFunc () , ageAbove18() , validEmailInput () , validPhoneFunc() , validPasswerdFunc ()) {
+    if (fLNameFunc () , ageAbove18() , validEmailInput () , validPhoneFunc() , validPasswerdFunc () ) {
         return true;
     }
     return false;
@@ -11,30 +11,33 @@ var regex = /^[a-zA-Z]+$/;
 function fLNameFunc () {
     regex = /^[a-zA-Z]+$/;
 if(regex.test(fName_input.value) && regex.test(lName_input.value)){
-    alert("valid name")
     return true;
 }
-alert("try again")
+first_label.innerHTML += "*<span>first name <b>MUST</b> be only in english:</span>";
+first_label.style.color = "red";
+last_label.innerHTML += "*<span>last name <b>MUST</b> be only in english:</span>";
+last_label.style.color = "red"
 return false;
 }
 
 function ageAbove18() {
     var maxDayDath = (new Date().getFullYear())-(input_Date.value.substr(0,4));
       if(maxDayDath>18){
-          alert("get in")
         return true;
       }
-      alert("go home")
+      
+      date_label.innerHTML += "*<span>Date of birth <b>must</b> be above18:</span>";
+      date_label.style.color = "red"
       return false;
 }
 
 function validEmailInput (){
     var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (regex.test(input_email.value)) {
-        alert("corect")
         return true;
     }
-    alert("wrong")
+    email_label.innerHTML += "*<span>Enter A valid Email:</span>";
+    email_label.style.color = "red"
     return false;
 
 }
@@ -42,10 +45,10 @@ function validEmailInput (){
 function validPhoneFunc () {
     var regexPhone = /^[050]\d+$/;
     if (regexPhone.test(phone_input.value)) {
-        alert("goodPhone")
         return true;
     }
-    alert("badPhone")
+    phone_label.innerHTML += "*<span>phone number <b>must</b> strat in 050 :</span>";
+    phone_label.style.color = "red"
     return false;
 
 }
@@ -53,10 +56,12 @@ var passD = passwerd1.value;
 function validPasswerdFunc () {
     var regexPass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,12})/;
     if ((regexPass.test(passwerd1.value))&&(passwerd1.value==passwerd2.value) && (passwerd1.value.toUpperCase([0])) && passwerd1.value.toLowerCase([1])) { 
-        alert("best");
         return true;
     }
-        alert("sedd");
+        pas_class1.innerHTML += "*<span>passwerd not storng enough:</span>";
+        pas_class2.innerHTML += "*<span>Passwerd are not match:</span>";
+        pas_class1.style.color = "red"
+        pas_class2.style.color = "red"
         return false;
         
     }
